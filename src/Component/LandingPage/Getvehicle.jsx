fa8c4da7adc4dea697252e956d507102ac35d9f1
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-12 17:10:27 -0400

    updating custom order

--- src/Component/LandingPage/Getvehicle.jsx
+++ src/Component/LandingPage/Getvehicle.jsx
@@ -1,20 +1,61 @@
 import React from 'react'
 import { data } from './data'
+import { IoIosArrowForward } from 'react-icons/io'
 
 const Getvehicle = () => {
     return (
-        <div className='inter'>
-            <div className='bg-[#303231] py-4 text-white font font-bold text-2xl'>
+        <div className='inter flex flex-col gap-10'>
+            <div className='bg-[#303231] py-4 text-white font font-bold text-2xl '>
                 <h1>Custom Order Your Mustang</h1>
             </div>
             <div className='container flex flex-col items-center'>
                 <p className='pt-6 text-[24px] font-semibold'>Get the vehicle you want with an online custom order.</p>
             </div>
-            <div>
-                {
-                    data.map()
-                }
-            </div>  
+            <div className='container grid grid-cols-3 pt-[2rem] w-full'>
+                <div className='flex items-center gap-[30px]'>
+                    <div className='w-[90%] h-[35vh] gap-3 justify-center flex flex-col items-center shadow-xl'>
+                        <div>
+                            <p className='p-4 flex items-center justify-center rounded-full w-[10px] h-[10px] bg-[#5046C7] text-white'>1</p>
+                        </div>
+                        <h1 className='font-bold'>Build & Price your Vehicle</h1>
+                        <p className='w-[65%] mt-4 text-[20px]'>Choose model, trim & options to create your custom order</p>
+                        <div className='flex items-center gap-2 mt-10'>
+                            <a className='text-[#FF6645] text-[15px]' href="">Learn more </a>
+                            <IoIosArrowForward className='w-[12px]' />
+                        </div>
+                    </div>
+                    <div className='w-[1px] bg-black h-[70%]'></div>
+                </div>
+                <div className='flex items-center gap-[30px]'>
+                    <div className='w-[90%] h-[35vh] gap-3 justify-center flex flex-col items-center shadow-xl'>
+                        <div>
+                            <p className='p-4 flex items-center justify-center rounded-full w-[10px] h-[10px] bg-[#FF6645] text-white'>2</p>
+                        </div>
+                        <h1 className='font-bold'>Share with your Delear</h1>
+                        <p className='w-[65%] mt-4 text-[20px]'>When you’re finished building, submit your vehicle to your local dealer.</p>
+                        <div className='flex items-center gap-2 mt-10'>
+                            <a className='text-[#FF6645] text-[15px]' href="">Learn more </a>
+                            <IoIosArrowForward className='w-[12px]' />
+                        </div>
+                    </div>
+                    <div className='w-[1px]  bg-black h-[70%]'></div>
+                </div>
+                <div className='w-[90%] h-[35vh] gap-3 justify-center flex flex-col items-center shadow-xl'>
+                    <div>
+                        <p className='p-4 flex items-center justify-center rounded-full w-[10px] h-[10px] bg-[#81D9F8] text-white'>1</p>
+                    </div>
+                    <h1 className='font-bold'>Place Your Order</h1>
+                    <p className='w-[70%] mt-4 text-[20px]'>Your dealer will contact you. Answer the questions before placing your order.</p>
+                    <div className='flex items-center gap-2 mt-10'>
+                        <a className='text-[#FF6645] text-[15px]' href="">Learn more </a>
+                        <IoIosArrowForward className='w-[12px]' />
+                    </div>
+                </div>
+            </div>
+            <div className='flex flex-col items-center gap-2'>
+                <button className='py-2 px-7 bg-[#003478] text-white'>Create your Account</button>
+                <u className='text-[#0276B3]'>Custom Order Explained</u>
+            </div>
         </div>
     )
 }
--- src/Component/LandingPage/data.js
+++ src/Component/LandingPage/data.js
@@ -2,20 +2,23 @@
 export const data = [
     {
         no: 1,
-        title: 'Build & Price your Vehicle',
+        title: '',
         paragraph: 'Choose model, trim & options to create your custom order',
-        learn: 'Learn More'
+        learn: 'Learn More',
+        color: ''
     },
     {
         no: 2,
         title: 'Share with your Delear',
         paragraph: 'When you’re finished building, submit your vehicle to your local dealer.',
-        learn: 'Learn More'
+        learn: 'Learn More',
+        color: ''
     },
     {
         no: 3,
         title: 'Place Your Order',
         paragraph: 'Your dealer will contact you. Answer the questions before placing your order.',
-        learn: 'Learn More'
+        learn: 'Learn More',
+        color:''
     }
 ]
