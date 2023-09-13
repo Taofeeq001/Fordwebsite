83ea04a5d17136e3832075063e8fd5feb2110921
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-13 18:12:02 -0400

    updating gallery section

--- src/Component/LandingPage/Gallery.jsx
+++ src/Component/LandingPage/Gallery.jsx
@@ -1,14 +1,34 @@
-import React from 'react';
+import React, { useState } from 'react';
 import car from '../../Images/car.svg'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io'
+import { gallery } from './data';
 
 const Gallery = () => {
+  const [currentImage, setCurrentImage] = useState(0)
+  
+  
+  
+  const handleforward = ()=>{
+    
+  }
+  const handleback = ()=>{
+
+  }
   return (
     <div className='bg-[#303231] font flex flex-col gap-9'>
-        <h1 className='text-white'>PHOTO GALLERY</h1>
+      <h1 className='text-white'>PHOTO GALLERY</h1>
+      <div className='relative w-full items-center justify-center flex flex-col'>
+        <img className='w-full h-[60vh]' src={gallery[currentImage].image} alt="" />
         <div>
-            <img src={car} alt="" />
+          <div onClick={handleforward} className='absolute top-[30%] right-[10%] text-white text-3xl rounded-[50%] border-2 border-white p-3'>
+            <IoIosArrowForward />
+          </div>
+          <div onClick={handleback} className=' absolute top-[30%] left-[10%] text-white text-3xl rounded-[50%] border-2 border-white p-3'>
+            <IoIosArrowBack />
+          </div>
         </div>
-        
+      </div>
+
     </div>
   )
 }
---
+++ src/Component/LandingPage/data.js
@@ -0,0 +1,32 @@
+import car from '../../Images/car.svg'
+import one from '../../Images/interior1.svg'
+import two from '../../Images/interior2.svg'
+import three from '../../Images/interior3.svg'
+import four from '../../Images/interior4.svg'
+import five from '../../Images/interior5.svg'
+import six from '../../Images/exterior.svg'
+
+
+export const gallery = [
+    {
+        image: <img src={car} alt='' />
+    },
+    {
+        image: <img src={one} alt='' />
+    },
+    {
+        image: <img src={two} alt='' />
+    },
+    {
+        image: <img src={three} alt='' />
+    },
+    {
+        image: <img src={four} alt='' />
+    },
+    {
+        image: <img src={five} alt='' />
+    },
+    {
+        image: <img src={six} alt='' />
+    }
+]
