af7b0de71dfef75bf3d824aaa1bd8a1a90fb5482
Taofeeq001 <taofeeqkolapo@gmail.com>
2023-09-18 11:22:05 -0400

    updating responsiveness

--- src/Component/LandingPage/Fordpass.jsx
+++ src/Component/LandingPage/Fordpass.jsx
@@ -7,7 +7,7 @@ const Fordpass = () => {
         <div className='flex lg:flex-row flex-col lg:gap-0 gap-7 lg:pl-[17%] justify-between items-center py-14 '>
             <div className='w-[65%] flex flex-col gap-10 text-left'>
                 <h1 className='text-4xl'>FordPass. Built to keep you Moving.®</h1>
-                <p className='inter text-[19px] leading-10'>FordPass Connect (optional on select vehicles), the FordPass App; and complimentary Connected Service are required for remote features (see FordPass Terms for details). Connected service and features depend on compatible AT&T network availability. Evolving technology/cellular networks/vehicle capability may limit functionality and prevent operation of connected features. Connected service excludes WiFi hotspot.</p>
+                <p className='inter text-[15px] lg:text-[19px] lg:text-start text-justify leading-10'>FordPass Connect (optional on select vehicles), the FordPass App; and complimentary Connected Service are required for remote features (see FordPass Terms for details). Connected service and features depend on compatible AT&T network availability. Evolving technology/cellular networks/vehicle capability may limit functionality and prevent operation of connected features. Connected service excludes WiFi hotspot.</p>
                 <div>
                     <button className='w-[274px] bg-[#003478] py-3 text-white hover:opacity-50'>Learn More</button>
                 </div>
--- src/Component/LandingPage/Power.jsx
+++ src/Component/LandingPage/Power.jsx
@@ -12,10 +12,10 @@ const Power = () => {
                 <p className='font text-4xl py-3'>POWER DOES MATTER</p>
             </div>
             <div className='flex w-full justify-center'>
-                <p className='inter text-2xl w-[68%]'>At the heart of every Mustang is a high-powered engine. Mustang offers four available engines, including the 5.2L supercharged V8.</p>
+                <p className='inter lg:text-2xl  w-full lg:w-[68%]'>At the heart of every Mustang is a high-powered engine. Mustang offers four available engines, including the 5.2L supercharged V8.</p>
             </div>
             <div className='container mt-7 flex justify-around w-full lg:flex-row flex-col items-center lg:gap-0 gap-4'>
-                <div className='lg:text-start flex flex-col gap-9 lg:w-[50%] w-full text-center lg:pl-0 pl-2'>
+                <div className='lg:text-start flex flex-col gap-9 lg:w-[50%] w-full  lg:pl-0 pl-2'>
                     <h1 className='text-5xl font-normal'>ExhoBoost®</h1>
                     <p className='lg:w-[57%] w-full font-semibold'>It’s a first Ecoboost® engine powered by Ford Performance and it’s not afraid to prove it. The 2.3L high-performance engine differentiates itself with a slightly larger twin-scroll turbo compressor.</p>
                     <div className='flex flex-col gap-5'>
@@ -41,7 +41,7 @@ const Power = () => {
                         </div>
                     </div>
                 </div>
-                <div className='lg:w-[40%] w-full justify-end'>
+                <div className='lg:w-[40%] w-full items-center lg:justify-end'>
                     <img src={eng1} alt="" />
                 </div>
             </div>
